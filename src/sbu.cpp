69b465781db3df69b82729479b778e812cbe998e
Ben Bucior <bbucior@u.northwestern.edu>
2016-09-07 15:35:47 -0500

    Cleanup code with helper functions
    Prepare the code for expansion (node detection, etc) by abstracting parts
    of it for simplification.  Still need to consider std::find for my helper
    function at the bottom.

--- src/sbu.cpp
+++ src/sbu.cpp
@@ -1,4 +1,5 @@
 // See https://openbabel.org/docs/dev/UseTheLibrary/CppExamples.html
+// Get iterator help from http://openbabel.org/dev-api/group__main.shtml
 
 #include <iostream>
 #include <stdio.h>
@@ -7,17 +8,18 @@
 #include <openbabel/mol.h>
 
 
-/* #define filename "../Data/RingCIFs/DOTSOV_clean.cif" */
-
 using namespace OpenBabel;  // See http://openbabel.org/dev-api/namespaceOpenBabel.shtml
 
+// Function prototypes
 bool readCIF(OBMol* molp, std::string filepath);
+void printFragments(const std::vector<std::string> &unique_smiles, bool display_number_of_fragments = false);
+bool stringInVector(const std::string &element, const std::vector<std::string> &vec);
+
 
 int main(int argc, char* argv[])
 {
 	// obErrorLog.SetOutputLevel(obDebug);  // See http://openbabel.org/wiki/Errors
 	const bool display_full_smiles = false;
-	const bool display_number_of_fragments = false;
 	char* filename;
 	filename = argv[1];  // TODO: Check usage later
 
@@ -33,50 +35,22 @@ int main(int argc, char* argv[])
 	OBConversion obconv;
 	obconv.SetOutFormat("can");
 	obconv.AddOption("i");  // Ignore SMILES chirality for now
-	std::vector<std::string> unique_smiles;
 
 	if (display_full_smiles) {
 		std::string whole_smiles = obconv.WriteString(&mol);
-		printf("Whole molecule: %s", whole_smiles.c_str());
+		printf("Whole molecule: %s", whole_smiles.c_str());  // WriteString already outputs a newline
 	}
 
-	// Print out each fragment and get a list of unique SMILES code
+	// Get a list of unique SMILES code
+	std::vector<std::string> unique_smiles;
 	for (std::vector<OBMol>::iterator it = fragments.begin(); it != fragments.end(); ++it) {
-		// Based on http://openbabel.org/dev-api/group__main.shtml
-		// printf("%d\t", it->NumAtoms());
 		std::string mol_smiles = obconv.WriteString(&*it);
-		// printf("%s", mol_smiles.c_str());  // WriteString already outputs a newline
-		bool new_flag = true;
-		for (std::vector<std::string>::iterator i2 = unique_smiles.begin(); i2 != unique_smiles.end(); ++i2) {
-			if (*i2 == mol_smiles) {
-				new_flag = false;
-				break;
-			}
-		}
-
-		if (new_flag) {
-			std::string chirality_flag;
-			if (it->IsChiral()) {
-				chirality_flag = "Chiral fragment";
-			} else {
-				chirality_flag = "Achiral fragment";
-			}  // It looks like the porphyrinic ADARAA defines the carboxylate as chiral
-			// printf("(New! %s)\n", chirality_flag.c_str());
+		if (!stringInVector(mol_smiles, unique_smiles)) {
 			unique_smiles.push_back(mol_smiles);
-		} else {
-			// printf("(Already defined)\n");
 		}
-
 	}
 
-	if (display_number_of_fragments) {
-		printf("\n\nFound %d fragments:\n", unique_smiles.size());
-	}
-
-
-	for (std::vector<std::string>::iterator i2 = unique_smiles.begin(); i2 != unique_smiles.end(); ++i2) {
-		printf("%s", i2->c_str());
-	}
+	printFragments(unique_smiles);
 
 	return(0);
 }
@@ -91,4 +65,26 @@ bool readCIF(OBMol* molp, std::string filepath) {
 	// Can disable bond detection as a diagnostic:
 	// obconversion.AddOption("s", OBConversion::INOPTIONS);
 	return obconversion.ReadFile(molp, filepath);
+}
+
+void printFragments(const std::vector<std::string> &unique_smiles, bool display_number_of_fragments) {
+	// Print a list of fragments, optionally with a count
+	if (display_number_of_fragments) {
+		printf("\n\nFound %d fragments:\n", unique_smiles.size());
+	}
+
+	// Use a const_iterator since we're not modifying the vector: http://stackoverflow.com/questions/4890497/how-do-i-iterate-over-a-constant-vector
+	for (std::vector<std::string>::const_iterator i2 = unique_smiles.begin(); i2 != unique_smiles.end(); ++i2) {
+		printf("%s", i2->c_str());
+	}
+}
+
+// template<class T>  // Consider using templates later, though there might be linker complications: https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl
+bool stringInVector(const std::string &element, const std::vector<std::string> &vec) {
+	for (std::vector<std::string>::const_iterator it = vec.begin(); it != vec.end(); ++it) {
+		if (*it == element) {
+			return true;
+		}
+	}
+	return false;
 }
