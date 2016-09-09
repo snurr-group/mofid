d2670021c7e151bc930b411ddc73970096ff9328
Ben Bucior <bbucior@u.northwestern.edu>
2016-09-09 10:10:51 -0500

    Extract node data by deleting linkers
    See top of sbu.cpp for next steps.  Node and linker identification is
    progressing nicely, though there's still lots of analysis left for validation.

--- src/sbu.cpp
+++ src/sbu.cpp
@@ -1,7 +1,10 @@
 // See https://openbabel.org/docs/dev/UseTheLibrary/CppExamples.html
 // Get iterator help from http://openbabel.org/dev-api/group__main.shtml
+// TODO: This works as a proof-of-concept, so then I'll need to fix the linker bonding,
+// tweak the element table for Zn4O connections, run tests, then analysis time!
 
 #include <iostream>
+#include <sstream>
 #include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +19,9 @@ using namespace OpenBabel;  // See http://openbabel.org/dev-api/namespaceOpenBab
 bool readCIF(OBMol* molp, std::string filepath);
 void printFragments(const std::vector<std::string> &unique_smiles, bool display_number_of_fragments = false);
 bool isMetal(const OBAtom* atom);
+void resetBonds(OBMol *mol);
+bool subtractMols(OBMol *mol, OBMol *subtracted);
+bool atomsEqual(const OBAtom &atom1, const OBAtom &atom2);
 
 
 template<typename T>  // WARNING: look out for linker complications: https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl
@@ -31,8 +37,9 @@ bool inVector(const T &element, const std::vector<T> &vec) {
 
 int main(int argc, char* argv[])
 {
-	// obErrorLog.SetOutputLevel(obDebug);  // See http://openbabel.org/wiki/Errors
-	const bool display_full_smiles = false;
+	obErrorLog.SetOutputLevel(obInfo);  // See also http://openbabel.org/wiki/Errors
+	const bool display_full_smiles = true;
+	const bool SAVE_NODES = true;
 	bool DELETE_BONDS = true;
 	char* filename;
 	filename = argv[1];  // TODO: Check usage later
@@ -42,12 +49,13 @@ int main(int argc, char* argv[])
 		printf("Error reading file: %s", filename);
 		exit(1);
 	}
+	OBMol orig_mol = mol;  // Copy original definition to another variable for later use
 
+	// Find linkers by deleting bonds to metals
 	std::vector<OBMol> fragments;
 	if (DELETE_BONDS) {
-		printf("Bond deletion enabled\n");
+		obErrorLog.ThrowError(__FUNCTION__, "Bond deletion enabled", obDebug);
 		// Same spirit as OBMol::DeleteHydrogens()
-		// std::set<OBBond*> delbonds;  // Can't actually use <set> and insert in STL because it's const
 		std::vector<OBBond*> delbonds;
 		FOR_ATOMS_OF_MOL(a, mol) {
 			if (isMetal(&*a)) {
@@ -56,19 +64,21 @@ int main(int argc, char* argv[])
 						// avoid double deletion in the case of metal-metal bonds
 						delbonds.push_back(&*b);
 					}
-					// See http://www.cplusplus.com/reference/set/set/insert/
 				}
 			}
 		}
 		mol.BeginModify();
+		std::stringstream deletionMsg;
 		for (std::vector<OBBond*>::iterator itb = delbonds.begin(); itb != delbonds.end(); ++itb) {
-			// TODO: Replace with OB debug statements throughout this module!
-			printf("Deleting bond with atomic numbers %d and %d (OBBond* %p)\n",
-					(*itb)->GetBeginAtom()->GetAtomicNum(),
-					(*itb)->GetEndAtom()->GetAtomicNum(),
-					static_cast<void *>(&*itb));
+			deletionMsg << "Deleting bond with atomic numbers ";
+			deletionMsg << (*itb)->GetBeginAtom()->GetAtomicNum() << " and ";
+			deletionMsg << (*itb)->GetEndAtom()->GetAtomicNum();
+			deletionMsg << " (OBBond * " << static_cast<void *>(&*itb) << ")\n";
 			mol.DeleteBond(*itb);  // FIXME: this correctly deletes the bonds, but the atomic valencies are messed up like my original version.
+			// IDEA: Maybe just need to rerun the ConnectTheDots type bond perception on the new fragments?
+			// That approach most works, though we should also consider preserving the connection point instead of outright deleting the bond
 		}
+		obErrorLog.ThrowError(__FUNCTION__, deletionMsg.str(), obDebug);
 		mol.EndModify();
 	}
 
@@ -79,21 +89,56 @@ int main(int argc, char* argv[])
 	obconv.SetOutFormat("can");
 	obconv.AddOption("i");  // Ignore SMILES chirality for now
 
-	if (display_full_smiles) {
-		std::string whole_smiles = obconv.WriteString(&mol);
-		printf("Whole molecule: %s", whole_smiles.c_str());  // WriteString already outputs a newline
-	}
-
 	// Get a list of unique SMILES code
 	std::vector<std::string> unique_smiles;
 	for (std::vector<OBMol>::iterator it = fragments.begin(); it != fragments.end(); ++it) {
-		std::string mol_smiles = obconv.WriteString(&*it);
+		OBMol canon = *it;
+		resetBonds(&canon);
+		std::string mol_smiles = obconv.WriteString(&canon);
 		if (!inVector<std::string>(mol_smiles, unique_smiles)) {
 			unique_smiles.push_back(mol_smiles);
 		}
 	}
 
 	printFragments(unique_smiles);
+
+	// Now try extracting the nonmetal components
+	// FIXME: currently a lot of copy-paste from the previous loop as proof-of-concept
+	OBMol nodes = orig_mol;  // TODO: consider renaming, or having separate fragments vars for SMILES
+	nodes.BeginModify();
+	for (std::vector<OBMol>::iterator it = fragments.begin(); it != fragments.end(); ++it) {
+		std::string mol_smiles = obconv.WriteString(&*it);
+		if (it->NumAtoms() == 1) {
+			// std::cout << "Found an oxygen!\n";
+			std::cout << "Found a solitary atom with atomic number " << it->GetFirstAtom()->GetAtomicNum() << std::endl;
+		//} else if () {
+		//	std::cout << "Hydroxyl group" << std::endl;
+		} else {
+			std::cout << "Deleting linker " << mol_smiles;
+			subtractMols(&nodes, &*it);
+		}
+		// Check for metal, single oxygens, etc
+		// For starters, this could probably be done with strings, then use actual atom types later
+		// Even better, try considering all single atoms and hydroxyl species as nodes.
+		// If it's not a linker, loop over atoms of the *it molecule and delete them from nodes.
+		// Then just extract nodes as what's left, as suggested in group meeting
+	}
+	nodes.EndModify();
+
+	if (display_full_smiles) {
+		std::string whole_smiles = obconv.WriteString(&orig_mol);
+		printf("Original molecule: %s", whole_smiles.c_str());
+		whole_smiles = obconv.WriteString(&mol);
+		printf("New molecule: %s", whole_smiles.c_str());  // WriteString already outputs a newline
+		whole_smiles = obconv.WriteString(&nodes);
+		printf("Nodes: %s", whole_smiles.c_str());  // WriteString already outputs a newline
+	}
+
+	if (SAVE_NODES) {
+		OBConversion node_conv;
+		node_conv.SetOutFormat("cif");  // mmcif has extra, incompatible fields
+		node_conv.WriteFile(&nodes, "test.cif");
+	}
 
 	return(0);
 }
@@ -136,3 +181,50 @@ bool isMetal(const OBAtom* atom) {
 	}
 	return true;  // Atom not classified as a nonmetal, so it's a metal
 }
+
+void resetBonds(OBMol *mol) {
+	// Resets bond orders and bond detection for molecular fragments
+	// FIXME: consider destroying all the bonds and starting from scratch
+	// Also see https://sourceforge.net/p/openbabel/mailman/message/6229244/
+	mol->ConnectTheDots();
+	mol->PerceiveBondOrders();
+}
+
+bool subtractMols(OBMol *mol, OBMol *subtracted) {
+	// Subtracts all atoms from a second molecule present in the first
+	// Returns true if successful, false if failure (extra atoms, etc)
+	// Modifies *mol if true, reverts back to original *mol if false
+	// This code assumes that the molecule is well-defined (no multiply-defined atoms)
+
+	std::vector<OBAtom*> deleted;  // TODO: consider implementing as a queue
+	FOR_ATOMS_OF_MOL(a2, *subtracted) {
+		bool matched = false;
+		FOR_ATOMS_OF_MOL(a1, *mol) {
+			if (atomsEqual(*a1, *a2)) {
+				deleted.push_back(&*a1);
+				matched = true;
+				break;
+			}
+		}
+		if (matched == false) {  // *subtracted has an extra atom
+			return false;  // *mol not modified
+		}
+	}
+
+	// It's okay to delete the atoms, since they should hold different (cloned) pointers
+	mol->BeginModify();
+	for (std::vector<OBAtom*>::iterator it = deleted.begin(); it != deleted.end(); ++it) {
+		mol->DeleteAtom(*it);
+	}
+	mol->EndModify();
+	return true;
+}
+
+bool atomsEqual(const OBAtom &atom1, const OBAtom &atom2) {
+	// Loosely based on the data copied during OBAtom::Duplicate
+	// Atoms are the same if these properties (excluding bond-related) are equivalent
+	return atom1.GetAtomicNum() == atom2.GetAtomicNum() &&
+			atom1.GetVector().IsApprox(atom2.GetVector(), 1.0e-6) &&
+			atom1.GetIsotope() == atom2.GetIsotope();
+}
+
