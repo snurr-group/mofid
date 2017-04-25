9c58fafed30ecb1e06b6fb50323d79f76a84fbee
Ben Bucior <bbucior@u.northwestern.edu>
2017-04-25 15:13:45 -0500

    Validate hMOFs against GA chromosomes
    Use the reduced WLLFHS hMOF database in Greg and Diego's GA paper
    (10.1126/sciadv.1600909) instead of the original work from Wilmer,
    since the composition and topologies are more clearly defined.  The
    base linkers are mostly implemented, though there work still remains
    on nitrogen-termination, Zn paddlewheels, etc.

--- Python/check_mof_linkers.py
+++ Python/check_mof_linkers.py
@@ -22,6 +22,7 @@ from extract_moffles import cif2moffles, assemble_moffles, parse_moffles, compar
 
 # Locations of important files, relative to the Python source code
 HMOF_DB = "../Resources/hmof_linker_info.json"
+GA_DB = "../Resources/ga_hmof_info.json"
 TOBACCO_DB = "../Resources/tobacco_info.json"
 KNOWN_DB = "../Resources/known_mof_info.json"
 
@@ -135,6 +136,7 @@ class KnownMOFs(MOFCompare):
 
 
 class HypoMOFs(MOFCompare):
+	# Original hypothetical MOFs from Wilmer 2012
 	def __init__(self):
 		self.db_file = path_to_resource(HMOF_DB)
 		self.load_components()
@@ -193,6 +195,103 @@ class HypoMOFs(MOFCompare):
 			return assemble_moffles(sbus, topology, mof_name=codes['name'])
 		else:
 			return None
+
+
+class GAMOFs(MOFCompare):
+	# Gene-based reduced WLLFHS hMOF database in Greg and Diego's 2016 paper
+	def __init__(self):
+		self.db_file = path_to_resource(GA_DB)
+		self.load_components()
+
+	def parse_filename(self, hmof_path):
+		# Extract hMOF recipes from the filename, formatted as xxxhypotheticalMOF_####_#_#_#_#_#_#.cif
+		codes = dict()
+		mof_name = basename(hmof_path)  # Get the basename without file extension
+
+		parts = mof_name.split("_")
+		assert len(parts) == 8  # hypoMOF + number + 6 chromosomes
+		codes["name"] = mof_name
+		codes["num"] = parts[1]
+		codes["max_cat"] = parts[2]
+		codes["cat"] = parts[3]
+		codes["nodes"] = parts[4]
+		codes["linker1"] = parts[5]
+		codes["linker2"] = parts[6]
+		codes["functionalization"] = parts[7]
+
+		return codes
+
+	def expected_moffles(self, cif_path):
+		# What is the expected MOFFLES based on the information in a MOF's filename?
+		# TODO: add catentation
+		codes = self.parse_filename(cif_path)
+		code_key = {
+			'nodes': 'nodes',
+			'linker1': 'linkers',
+			'linker2': 'linkers',
+			'functionalization': 'functionalization',
+		}
+
+		if codes['functionalization'] != "0" or codes["linker1"] != codes["linker2"]:
+			return None  # For now, temporarily exclude functionalization (until we can figure out SMIRKS transforms)
+			# Also exclude multiple linkers, to narrow down the number of test structures, at least to begin
+
+		is_component_defined = []
+		for key in code_key:
+			#is_component_defined.extend([x in self.mof_db[code_key[key]] for x in codes[key]])
+			is_component_defined.extend([codes[key] in self.mof_db[code_key[key]]])
+
+
+		if not any(False, is_component_defined):  # Everything is defined.  Why didn't I use Python's built-in `all`?  Test this later.
+			sbus = []
+			sbu_codes = ['nodes', 'linker1', 'linker2']  # TODO: implement functionalization and N-termination
+			for part in sbu_codes:
+				smiles = self.mof_db[code_key[part]][codes[part]]
+				if smiles not in sbus:
+					sbus.append(smiles)
+			sbus.sort()
+
+			topology = self._topology_from_gene(codes)
+			return assemble_moffles(sbus, topology, mof_name=codes['name'])
+		else:
+			return None
+
+	def _topology_from_gene(self, genes):
+		# Expected topology based on the gene-based identification criteria in Table S1 of 10.1126/sciadv.1600909
+		zn4o = 0
+		paddlewheels = [1, 2]
+		v_node = 3
+		zr_node = 4
+		tritopic = range(30, 34)  # (30-33)
+		tetrahedral4 = range(34, 38)  # tetrahedral tetratopic linkers
+		planar4 = [38, 39]  # planar tetratopic linkers
+
+		nodes = int(genes["nodes"])
+		linker1 = int(genes["linker1"])
+		linker2 = int(genes["linker2"])
+
+		if nodes == zn4o:  # Zn4O nodes
+			return "pcu"
+		elif nodes in paddlewheels and linker1 < 30 and linker2 < 30:  # Paddlewheels with ditopic linkers
+			return "pcu"
+		elif nodes == zr_node and linker1 != linker2:  # Zr nodes and two different linkers
+			return "pcu"
+
+		elif nodes == v_node:  # Vanadium nodes
+			return "ERROR"  # FIXME: will be **sra** or something similar once implemented
+		elif nodes == zr_node and linker1 == linker2:  # Zr nodes and one type of linker
+			return "fcu"
+
+		elif linker1 in tritopic and linker2 in tritopic:
+			return "tbo"
+		elif linker1 in tetrahedral4 and linker2 in tetrahedral4:
+			return "dia"
+		elif linker1 in planar4 and linker2 in planar4:
+			return "nbo"
+
+		else:
+			raise ValueError("Undefined topology for " + genes["name"])
+			return "UNK"
 
 
 class TobaccoMOFs(MOFCompare):
@@ -264,6 +363,7 @@ class AutoCompare:
 		self.known = KnownMOFs()
 		self.precalculated = self.known.mof_db.keys()
 		self.hmof = HypoMOFs()
+		self.ga = GAMOFs()
 		self.tobacco = TobaccoMOFs()
 		# Maybe also a NullMOFs class eventually, which is just a calculator sans comparisons?
 
@@ -274,8 +374,12 @@ class AutoCompare:
 			mof_log("...using precompiled table of known MOFs\n")
 			return self.known.test_cif(cif_path)
 		elif "hypotheticalmof" in mof_info.lower() or "hmof" in mof_info.lower():
-			mof_log("...parsing file with rules for hypothetical MOFs\n")
-			return self.hmof.test_cif(cif_path)
+			if "_i_" in mof_info.lower():
+				mof_log("...parsing file with rules for Wilmer hypothetical MOFs\n")
+				return self.hmof.test_cif(cif_path)
+			else:
+				mof_log("...parsing file with rules for GA hypothetical MOFs\n")
+				return self.ga.test_cif(cif_path)
 		elif "_sym_" in mof_info:
 			mof_log("...parsing file with rules for ToBACCo MOFs\n")
 			return self.tobacco.test_cif(cif_path)
