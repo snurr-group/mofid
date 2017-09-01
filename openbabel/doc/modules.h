2349c5d2f2e555260d4df4d67e94202ce763a8c2
Ben Bucior <bbucior@u.northwestern.edu>
2017-09-01 16:55:09 -0500

    Remove manually copied Open Babel distribution
    Open Babel had been manually imported from the upstream project ref
    03fa0761 on 6/23/16 into MOFid ref 7810ca7b.  This commit undoes this
    directory (and changes like PBC since then) to prepare for a cleaner,
    more modular `git subtree` approach.  Note that the script is not
    included with Cygwin's distribution of git, so Git Bash will be
    required.  Future changes to upstream Open Babel code then can be more
    easily integrated back into the MOFid work (e.g. hopefully a more
    robust aromaticity model).

--- openbabel/doc/modules.h
+++
@@ -1,9 +0,0 @@
-/**
- * @defgroup main Getting Started
- * @defgroup stereo Stereochemistry
- * @defgroup conformer Conformer Searching
- * @defgroup substructure Substructure Searching
- */
-
-/// @file modules.h
-/// @brief Doxygen modules declaration.
