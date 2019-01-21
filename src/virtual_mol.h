e64f5cecef23c17313df52d0fd59ef2dfd109864
Ben Bucior <bbucior@u.northwestern.edu>
2019-01-21 10:20:17 -0600

    Copy VirtualMol's to a MappedMol for easier tracking
    This commit will set up some more of the groundwork to facilitate writing the All-Node algorithm.

--- src/virtual_mol.h
+++ src/virtual_mol.h
@@ -22,9 +22,10 @@ typedef std::set< std::pair<OBAtom*, OBAtom*> > ConnIntToExt;
 typedef std::set<OBAtom*> AtomSet;  // TODO consider using this alias throughout
 
 class VirtualMol;
+class MappedMol;
 
 class VirtualMol {
-// A collection of OBAtom*'s in a parent molecule.
+// A lightweight collection of OBAtom*'s in a parent molecule.
 // The alternative of copying OBMol's is more complicated, because that generates new OBAtom
 // instances and it's more difficult to compare two OBAtom's if they intrinsically have different raw pointers.
 // (Formerly, the code instead relied on searching for identical atomic number, position, etc.)
@@ -47,6 +48,7 @@ public:
 	// Imports an OBMol fragment with copies of atoms in the same positions as _parent_mol
 	int ImportCopiedFragment(OBMol *fragment);
 	ConnIntToExt GetExternalBondsOrConns();  // map of external connections in the parent molecule
+	void CopyToMappedMol(MappedMol *dest, bool export_bonds = true, bool copy_bonds = true);
 	OBMol ToOBMol(bool export_bonds = true, bool copy_bonds = true);
 	void ToCIF(const std::string &filename, bool write_bonds = true);
 	// TODO: consider implementing SMILES in a parent class due to OBConv
@@ -62,6 +64,30 @@ public:
 #define FOR_RW_ATOMS_OF_VMOL(a,v) \
 	AtomSet __vset = v.GetAtoms(); \
 	for (AtomSet::iterator a = __vset.begin(); a != __vset.end(); ++a)
+
+
+typedef std::map<OBAtom*, OBAtom*> atom_map_t;
+
+class MappedMol {
+// Copy of an OBMol, including a 1:1 mapping between the origin and copied OBAtom's.
+// WARNING: this class exposes the underlying OBMol and will NOT automatically keep track
+// of changes to the mappings.  It's just a convenient struct to hold this data.
+// It also does not have any initialization--that's the role of methods like VirtualMol::ToMappedMol()
+private:
+	// Make the class noncopyable to avoid issues with OBMol pointer ownership
+	// or changes to OBAtom*'s when copying an OBMol
+	MappedMol(const MappedMol& other);
+	MappedMol& operator=(const MappedMol&);
+
+public:
+	OBMol mol_copy;
+	OBMol* origin_molp;
+	atom_map_t origin_to_copy;
+	atom_map_t copy_to_origin;
+
+	MappedMol() { origin_molp = NULL; };
+	virtual ~MappedMol() {};
+};
 
 
 } // end namespace OpenBabel
