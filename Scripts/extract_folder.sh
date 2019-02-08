ac694af9b88b64bdde06e3aa341c91f1745aeb69
Ben Bucior <bbucior@u.northwestern.edu>
2019-02-08 16:00:38 -0600

    Improve documentation of extract_folder.sh
    Explicitly parsing input parameters, adding comments about its purpose, etc.

--- Scripts/extract_folder.sh
+++ Scripts/extract_folder.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+# Runs the Python MOFid analysis on a directory of CIFs
+#
+# Outputs the a MOFids in the .smi SMILES format to stdout and any logging
+# info, such as warnings from sbu.exe or the current progress, to stderr.
+#
+# The intermediate OUTPUT_DIR is temporary and overwritten by each MOF,
+# but it is included as a parameter to allow multiple copies of run_folder.sh
+# to run concurrently, such as parallel jobs on an HPC cluster.  Otherwise,
+# multiple processes would be writing to the same intermediate topology folder
+# and possibly cause inconsistencies.
 
-OUTPUT_COPY="$2/results_part.txt"
-mkdir -p "$2"
+# Parse input arguments
+CIF_DIR="$1"
+OUTPUT_DIR="$2"
 
-echo "Analyzing $1 with MOFid commit:" 1>&2
+OUTPUT_COPY="${OUTPUT_DIR}/results_part.txt"
+mkdir -p "${OUTPUT_DIR}"
+
+echo "Analyzing ${CIF_DIR} with MOFid commit:" 1>&2
 git rev-parse --verify HEAD 1>&2
-echo "Writing results to intermediate directory: $2" 1>&2
+echo "Writing results to intermediate directory: ${OUTPUT_DIR}" 1>&2
 echo "----------------------------" 1>&2
 echo "Time:" 1>&2
 date 1>&2
 echo "----------------------------" 1>&2
 
-for i in "$1/"*.cif
+for i in "${CIF_DIR}/"*.cif
 do
 	echo "Beginning $i" 1>&2
 	echo "----------------------------" 1>&2
-	python Python/extract_moffles.py $i "$2" | tee -a $OUTPUT_COPY
+	python Python/extract_moffles.py "$i" "${OUTPUT_DIR}" | tee -a "${OUTPUT_COPY}"
 done
 
 echo "----------------------------" 1>&2
