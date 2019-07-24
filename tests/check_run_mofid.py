ca1d7c8a1bd34780f51e1d71e4a0367c46250619
Andrew S. Rosen <rosen@u.northwestern.edu>
2019-07-23 20:36:51 -0500

    Update .py test

--- tests/check_run_mofid.py
+++ tests/check_run_mofid.py
@@ -2,7 +2,5 @@ import os
 from mofid.run_mofid import cif2mofid
 from mofid.paths import resources_path
 
-test_mofs = os.path.join(resources_path,'TestCIFs')
-mof1 = os.listdir(test_mofs)[0]
-cif_path = os.path.join(test_mofs,mof1)
+cif_path = os.path.join(resources_path,'ABAVIJ_clean.cif')
 mofid = cif2mofid(cif_path)
