a1e1eaeb42844cca2e970328813f95c0202a3d4a
Andrew S. Rosen <rosen@u.northwestern.edu>
2019-07-23 20:38:11 -0500

    Finish update to .py test
    More explicitly confirm correct mofid/mofkey

--- tests/check_run_mofid.py
+++ tests/check_run_mofid.py
@@ -2,5 +2,8 @@ import os
 from mofid.run_mofid import cif2mofid
 from mofid.paths import resources_path
 
-cif_path = os.path.join(resources_path,'ABAVIJ_clean.cif')
+cif_path = os.path.join(resources_path,'TestCIFs','ABAVIJ_clean.cif')
 mofid = cif2mofid(cif_path)
+if (mofid['mofkey'] != 'Co.TWBYWOBDOCUKOW.MOFkey-v1.rtl'
+	or mofid['mofid'] != '[Co].[O-]C(=O)c1ccncc1 MOFid-v1.rtl.cat0;ABAVIJ_clean'):
+	raise ValueError('Test failed!')
