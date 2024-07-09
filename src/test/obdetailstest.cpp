39053468f54926e90c49c112fd3f5765eff960ff
dcheng <102439573+dcheng2022@users.noreply.github.com>
2024-07-09 15:45:56 -0500

    add OBDetails unit test

--- src/test/obdetailstest.cpp
+++ src/test/obdetailstest.cpp
@@ -4,18 +4,33 @@
 
 #include "obdetails.h"
 
+namespace OBDetailsTest {
+    constexpr int numElements{118};
+    constexpr int numNonMetals{23};
+    constexpr std::array<int, numNonMetals> nonMetalAtomicNums{1, 2, 5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 32, 33, 34, 35, 36, 52, 53, 54, 85, 86};
+}
+
+using namespace OBDetailsTest;
 
 TEST(IsMetalTest, HandlesMetal) {
+    int idx{0};
+    OpenBabel::OBAtom metalAtom{};
+    for (std::size_t atomicNum{1}; atomicNum <= numElements; ++atomicNum) {
+        if ((idx < numNonMetals) && (atomicNum == nonMetalAtomicNums[idx]))
+            ++idx;
+        else {
+            metalAtom.SetAtomicNum(atomicNum);
+            const OpenBabel::OBAtom metalAtom2{metalAtom};
+            EXPECT_TRUE(OpenBabel::isMetal(&metalAtom2));
+        }
+    }
 }
 
 TEST(IsMetalTest, HandlesNonMetal) {
-    constexpr int numNonMetals{23};
-    constexpr std::array<int, numNonMetals> nonMetalAtomicNums{1, 2, 5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 32, 33, 34, 35, 36, 52, 53, 54, 85, 86};
     OpenBabel::OBAtom nonMetalAtom{};
     for (int atomicNum : nonMetalAtomicNums) {
         nonMetalAtom.SetAtomicNum(atomicNum);
         const OpenBabel::OBAtom nonMetalAtom2{nonMetalAtom};
-        const OpenBabel::OBAtom* nonMetalAtomPtr{&nonMetalAtom2};
-        EXPECT_FALSE(OpenBabel::isMetal(nonMetalAtomPtr));
+        EXPECT_FALSE(OpenBabel::isMetal(&nonMetalAtom2));
     }
 }
