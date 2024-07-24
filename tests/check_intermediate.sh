c856440117ebba9b08e0afb1c29b3290c57489b7
dcheng <102439573+dcheng2022@users.noreply.github.com>
2024-07-24 13:17:42 -0500

    add verbose flag to check_intermediate

--- tests/check_intermediate.sh
+++ tests/check_intermediate.sh
@@ -5,11 +5,24 @@ GREEN="\033[0;92m"
 RED="\033[0;31m"
 NC="\033[0m" # No color
 
+# FLAGS
+verbose=false
+
+while getopts "v" flag; do
+    case $flag in
+        v) verbose=true ;;
+    esac
+done
+
 for path_to_cif in Resources/KnownCIFs/*.cif; do
     cif="$(basename $path_to_cif | sed 's/.cif//g')"
     echo -e "${GREEN}RUNNING${NC} bin/sbu $path_to_cif Output/$cif..."
-    # TODO: make sbu output condition on verbose flag
-    bin/sbu $path_to_cif Output/$cif &> /dev/null
+    if [[ $verbose = true ]]; then
+        bin/sbu $path_to_cif Output/$cif 
+        echo "\n"
+    else
+        bin/sbu $path_to_cif Output/$cif &> /dev/null
+    fi
 done
 for path_to_cif in Resources/KnownCIFs/*.cif; do
     cif="$(basename $path_to_cif | sed 's/.cif//g')"
@@ -23,19 +36,28 @@ for path_to_cif in Resources/KnownCIFs/*.cif; do
     fi
     for path_to_dir in $(find $known_path/* -type d); do
         dir="$(basename $path_to_dir)"
-        # In order, ELEMENT# -> ELEMENT, NODE# -> "", Lines with # -> "", # -> 1 decimal place
+        # In order, a-zA-Z# -> a-zA-Z, NODE# -> "", Lines with # -> "", # -> 1 decimal place
         pattern="s/([a-zA-Z]+)[0-9]+/\1/g; s/NODE [0-9]+/NODE/g; s/^[ \t]*#.*//g; s/([0-9]+)(.[0-9])?[0-9]+/\1\2/g"
         for path_to_file in $path_to_dir/*; do
             file="$(basename $path_to_file)"
             diff -b <(sed -E "$pattern" $path_to_file | sort) <(sed -E "$pattern" $output_path/$dir/$file | sort) > /dev/null
-            if [[ $? -ne 0 ]]; then
+            exit_code="$?"
+            if [[ $exit_code -ne 0 ]] && [[ $verbose = true ]] ; then
                 echo -e "${RED}WARNING:${NC} $cif/$dir/$file is different"
                 diff -yb --suppress-common-lines <(sed -E "$pattern" $path_to_file | sort) <(sed -E "$pattern" $output_path/$dir/$file | sort) | colordiff
+            elif [[ $exit_code -ne 0 ]]; then
+                # This pattern tests simpler equality
+                simple_pattern="s/[ \t0-9.\-]e?//g; s/^[ \t]*#.*//g;"
+                diff -b <(sed -E "$simple_pattern" $path_to_file | sort) <(sed -E "$simple_pattern" $output_path/$dir/$file | sort) > /dev/null
+                if [[ $? -ne 0 ]]; then
+                    diff -yb --suppress-common-lines <(sed -E "$simple_pattern" $path_to_file | sort) <(sed -E "$simple_pattern" $output_path/$dir/$file | sort) | colordiff
+                else
+                    echo -e "${GREEN}SUCCESS:${NC} $cif/$dir/$file is identical"
+                fi
             else
                 echo -e "${GREEN}SUCCESS:${NC} $cif/$dir/$file is identical"
             fi
         done
     done
 done
-# TODO: remove this once the script is complete
 exit 0
