793d8ec4b8ce290d63c7a549f43343287131693d
dcheng <102439573+dcheng2022@users.noreply.github.com>
2024-08-19 15:51:44 -0500

    overhaul atom and bond comparisons

--- src/compare.cpp
+++ src/compare.cpp
@@ -10,7 +10,6 @@
 #include <openbabel/obconversion.h>
 #include <openbabel/generic.h>
 #include <openbabel/atom.h>
-#include <openbabel/bond.h>
 #include <openbabel/math/vector3.h>
 
 #include "config_sbu.h"
@@ -18,8 +17,7 @@
 
 using namespace OpenBabel;
 
-int main(int argc, char* argv[]) 
-{
+int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Incorrect number of arguments. Need to specify two CIFs." << std::endl;
         return 2;
@@ -44,6 +42,7 @@ OBMol getMolFromCIF(const std::string& pathToFile) {
     std::fstream cif{pathToFile};
     OBConversion obconversion(&cif);
     obconversion.SetInFormat("CIF");
+    obconversion.SetOptions("Bbs", OBConversion::INOPTIONS);
     OBMol mol{};
     obconversion.Read(&mol);
     return mol;
@@ -52,19 +51,15 @@ OBMol getMolFromCIF(const std::string& pathToFile) {
 bool areMolsSame(OBMol* mol1, OBMol* mol2) {
     bool isIdentical{true};
     if (!(mol1->NumAtoms() == mol2->NumAtoms())) {
-        std::cout << "Molecular atom count mismatch" << std::endl;
+        std::cout << "Molecular atom count mismatch" << " mol1: " << mol1->NumAtoms() << " mol2: " << mol2->NumAtoms() << std::endl;
+        isIdentical = false;
+    }
+    if (!(mol1->NumBonds() == mol2->NumBonds())) {
+        std::cout << "Molecular bond count mismatch" << " mol1: " << mol1->NumBonds() << " mol2: " << mol2->NumBonds() << std::endl;
         isIdentical = false;
     }
     if (mol1->GetFormula() != mol2->GetFormula()) {
-        std::cout << "Molecular stoichiometric formula mismatch" << std::endl;
-        isIdentical = false;
-    }
-    if (mol1->GetEnergy() != mol2->GetEnergy()) {
-        std::cout << "Molecular energy mismatch" << std::endl;
-        isIdentical = false;
-    }
-    if (mol1->GetMolWt() != mol2->GetMolWt()) {
-        std::cout << "Molecular weight mismatch" << std::endl;
+        std::cout << "Molecular stoichiometric formula mismatch" << " " << mol1->GetFormula() << " " << mol2->GetFormula() << std::endl;
         isIdentical = false;
     }
     OBUnitCell* mol1UC{static_cast<OBUnitCell*>(mol1->GetData(OBGenericDataType::UnitCell))};
@@ -76,62 +71,51 @@ bool areMolsSame(OBMol* mol1, OBMol* mol2) {
     if (!isIdentical) {
         return false;
     } else{
-        return areBondsSame(mol1, mol2);
+        return areAtomsSame(mol1, mol2, 3);
     }
 }
 
-bool areBondsSame(OBMol* mol1, OBMol* mol2) {
-    bool isIdentical{true};
-    if (!(mol1->NumBonds() == mol2->NumBonds())) {
-        isIdentical = false;
+bool areAtomsSame(OBMol* mol1, OBMol* mol2, int precision) {
+    OBAtomIterator mol1Atoms{mol1->BeginAtoms()};
+    const OBAtomIterator mol1AtomsEnd{mol1->EndAtoms()};
+    std::map<unsigned int, std::set<OBAtom*>> atomicNumToAtoms{};
+    while (mol1Atoms != mol1AtomsEnd) {
+        OBAtom* mol1Atom{*mol1Atoms};
+        unsigned int atomicNum{mol1Atom->GetAtomicNum()};
+        if (auto search = atomicNumToAtoms.find(atomicNum); search != atomicNumToAtoms.end()) {
+            atomicNumToAtoms[atomicNum].insert(mol1Atom);
+        } else {
+            std::set<OBAtom*> atoms{mol1Atom};
+            atomicNumToAtoms.insert(std::make_pair(atomicNum, atoms));
+        }
+        ++mol1Atoms;
     }
-    OBBondIterator mol1Bonds{mol1->BeginBonds()};
-    const OBBondIterator mol1BondsEnd{mol1->EndBonds()};
-    std::map<std::set<std::vector<double>>, OBBond*> coordsToBond{};
-    while (mol1Bonds != mol1BondsEnd) {
-        OBBond* mol1Bond{*mol1Bonds};
-        OBAtom* mol1BeginAtom{mol1Bond->GetBeginAtom()};
-        OBAtom* mol1EndAtom{mol1Bond->GetEndAtom()};
-        std::set<std::vector<double>> coords{{mol1BeginAtom->GetX(), mol1BeginAtom->GetY(), mol1BeginAtom->GetZ()}, {mol1EndAtom->GetX(), mol1EndAtom->GetY(), mol1EndAtom->GetZ()}};
-        coordsToBond.insert(std::make_pair(coords, mol1Bond));
-        ++mol1Bonds;
-    }
-    OBBondIterator mol2Bonds{mol2->BeginBonds()};
-    const OBBondIterator mol2BondsEnd{mol2->EndBonds()};
-    while (mol2Bonds != mol2BondsEnd) {
-        OBBond* mol2Bond{*mol2Bonds};
-        OBAtom* mol2BeginAtom{mol2Bond->GetBeginAtom()};
-        OBAtom* mol2EndAtom{mol2Bond->GetEndAtom()};
-        std::set<std::vector<double>> coords{{mol2BeginAtom->GetX(), mol2BeginAtom->GetY(), mol2BeginAtom->GetZ()}, {mol2EndAtom->GetX(), mol2EndAtom->GetY(), mol2EndAtom->GetZ()}};
-        if (coordsToBond.count(coords) > 0) {
-            OBBond* mol1Bond{coordsToBond[coords]};
-            if (!isClose(mol1Bond->GetBondOrder(), mol2Bond->GetBondOrder())) {
-                std::cout << "Bond order mismatch" << std::endl;
-                isIdentical = false;
+    OBAtomIterator mol2Atoms{mol2->BeginAtoms()};
+    const OBAtomIterator mol2AtomsEnd{mol2->EndAtoms()};
+    while (mol2Atoms != mol2AtomsEnd) {
+        OBAtom* mol2Atom{*mol2Atoms};
+        unsigned int atomicNum{mol2Atom->GetAtomicNum()};
+        if (auto search = atomicNumToAtoms.find(atomicNum); search != atomicNumToAtoms.end()) {
+            bool foundMatch{false};
+            for (OBAtom* mol1Atom : search->second) {
+                if (isAtomSame(mol1, mol1Atom, mol2, mol2Atom)) {
+                    foundMatch = true;
+                    search->second.erase(mol1Atom);
+                    break;
+                }
             }
-            if (!isClose(mol1Bond->GetLength(), mol2Bond->GetLength())) {
-                std::cout << "Bond length mismatch" << std::endl;
-                isIdentical = false;
-            }
-            // std::cout << "Bond: " << "(" << mol1Bond->GetBeginAtom()->GetVector() << " " << mol1Bond->GetEndAtom()->GetVector() << ") (" << mol2Bond->GetBeginAtom()->GetVector() << " " << mol2Bond->GetEndAtom()->GetVector() << ")" << std::endl;
-            if (!(isAtomSame(mol1Bond->GetBeginAtom(), mol2Bond->GetBeginAtom()) || isAtomSame(mol1Bond->GetBeginAtom(), mol2Bond->GetEndAtom()))) {
-                std::cout << "Bond begin atom mismatch" << std::endl;
-                isIdentical = false;
-            }
-            if (!(isAtomSame(mol1Bond->GetEndAtom(), mol2Bond->GetBeginAtom()) || isAtomSame(mol1Bond->GetEndAtom(), mol2Bond->GetEndAtom()))) {
-                std::cout << "Bond end atom mismatch" << std::endl;
-                isIdentical = false;
+            if (!foundMatch) {
+                std::cout << "Atom match not found " << mol2Atom->GetAtomicNum() << std::endl;
             }
         } else {
-            std::cout << "Bond mismatch" << std::endl;
-            isIdentical = false;
+            std::cout << "Atomic number " << atomicNum << " not found" << std::endl;
         }
-        ++mol2Bonds;
+        ++mol2Atoms;
     }
-    return isIdentical;
+    return true;
 }
 
-bool isAtomSame(OBAtom* atom1, OBAtom* atom2) {
+bool isAtomSame(OBMol* mol1, OBAtom* atom1, OBMol* mol2, OBAtom* atom2) {
     if (atom1->GetAtomicNum() != atom2->GetAtomicNum()) {
         return false;
     }
@@ -141,10 +125,81 @@ bool isAtomSame(OBAtom* atom1, OBAtom* atom2) {
     if (atom1->GetIsotope() != atom2->GetIsotope()) {
         return false;
     }
-    if (atom1->GetTotalDegree() != atom2->GetTotalDegree()) {
+    if (!areBondsSame(mol1, atom1, mol2, atom2)) {
         return false;
     }
     return true;
+}
+
+std::vector<OBBond*> getBonds(OBMol* mol, OBAtom* atom) {
+    OBBondIterator molBonds{mol->BeginBonds()};
+    const OBBondIterator molBondsEnd{mol->EndBonds()};
+    std::vector<OBBond*> bonds{};
+    while (molBonds != molBondsEnd) {
+        OBBond* molBond{*molBonds};
+        if ((molBond->GetBeginAtom() == atom) || (molBond->GetEndAtom() == atom)) {
+            bonds.push_back(molBond);
+        }
+        ++molBonds;
+    }
+    return bonds;
+}
+
+bool areBondsSame(OBMol* mol1, OBAtom* atom1, OBMol* mol2, OBAtom* atom2) {
+    std::map<unsigned int, int> atomicNumToCount{};
+    for (OBBond* atom1Bond : getBonds(mol1, atom1)) {
+        OBAtom* endAtom{};
+        if (atom1Bond->GetBeginAtom() != atom1) {
+            endAtom = atom1Bond->GetBeginAtom();
+        } else {
+            endAtom = atom1Bond->GetEndAtom();
+        }
+        unsigned int atomicNum{endAtom->GetAtomicNum()};
+        if (auto search = atomicNumToCount.find(atomicNum); search != atomicNumToCount.end()) {
+            ++atomicNumToCount[atomicNum];
+        } else {
+            atomicNumToCount.insert(std::make_pair(atomicNum, 1));
+        }
+    }
+    for (OBBond* atom2Bond : getBonds(mol2, atom2)) {
+        OBAtom* endAtom{};
+        if (atom2Bond->GetBeginAtom() != atom2) {
+            endAtom = atom2Bond->GetBeginAtom();
+        } else {
+            endAtom = atom2Bond->GetEndAtom();
+        }
+        unsigned int atomicNum{endAtom->GetAtomicNum()};
+        if (auto search = atomicNumToCount.find(atomicNum); search != atomicNumToCount.end()) {
+            --atomicNumToCount[atomicNum];
+            if (atomicNumToCount[atomicNum] == 0) {
+                atomicNumToCount.erase(atomicNum);
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+void printBonds(OBMol* mol, const std::string& name, int precision) {
+    OBBondIterator molBonds{mol->BeginBonds()};
+    const OBBondIterator molBondsEnd{mol->EndBonds()};
+    while (molBonds != molBondsEnd) {
+        printBond(*molBonds, name, precision); 
+        ++molBonds;
+    }
+}
+
+void printBond(OBBond* bond, const std::string& name, int precision) {
+        OBAtom* beginAtom{bond->GetBeginAtom()};
+        int beginAtomX{static_cast<int>(std::trunc(beginAtom->GetX() * std::pow(10, precision)))};
+        int beginAtomY{static_cast<int>(std::trunc(beginAtom->GetY() * std::pow(10, precision)))};
+        int beginAtomZ{static_cast<int>(std::trunc(beginAtom->GetZ() * std::pow(10, precision)))};
+        OBAtom* endAtom{bond->GetEndAtom()};
+        int endAtomX{static_cast<int>(std::trunc(endAtom->GetX() * std::pow(10, precision)))};
+        int endAtomY{static_cast<int>(std::trunc(endAtom->GetY() * std::pow(10, precision)))};
+        int endAtomZ{static_cast<int>(std::trunc(endAtom->GetZ() * std::pow(10, precision)))};
+        std::cout << name << ": (" << beginAtomX << " " << beginAtomY << " " << beginAtomZ << ") (" << endAtomX << " " << endAtomY << " " << endAtomZ << ") order: " << bond->GetBondOrder() << std::endl;
 }
 
 bool isClose(double A, double B) {
--- src/compare.h
+++ src/compare.h
@@ -1,16 +1,22 @@
 #ifndef COMPARE_H
 #define COMPARE_H
 
+#include <vector>
 #include <string>
 
 #include <openbabel/mol.h>
+#include <openbabel/bond.h>
 
 using namespace OpenBabel;
 
 OBMol getMolFromCIF(const std::string& pathToFile);
 bool areMolsSame(OBMol* mol1, OBMol* mol2);
-bool areBondsSame(OBMol* mol1, OBMol* mol2);
-bool isAtomSame(OBAtom* atom1, OBAtom* atom2);
+bool areAtomsSame(OBMol* mol1, OBMol* mol2, int precision);
+bool isAtomSame(OBMol* mol1, OBAtom* atom1, OBMol* mol2, OBAtom* atom2);
+std::vector<OBBond*> getBonds(OBMol* mol, OBAtom* atom);
+bool areBondsSame(OBMol* mol1, OBAtom* atom1, OBMol* mol2, OBAtom* atom2);
+void printBonds(OBMol* mol, const std::string& name, int precision);
+void printBond(OBBond* bond, const std::string& name, int precision);
 bool isClose(double A, double B);
 
 #endif
